4eb6ecf0ba84b026de85add243833414c83d4fbe
Matt Driscoll <mdriscoll@esri.com>
2018-03-03 18:05:14 -0800

    cleanup

--- demos/bookmarks-complete/app/Bookmarks/BookmarksViewModel.ts
+++ demos/bookmarks-complete/app/Bookmarks/BookmarksViewModel.ts
@@ -1,11 +1,7 @@
 /// <amd-dependency path="esri/core/tsSupport/declareExtendsHelper" name="__extends" />
 /// <amd-dependency path="esri/core/tsSupport/decorateHelper" name="__decorate" />
 
-import {
-  declared,
-  property,
-  subclass
-} from "esri/core/accessorSupport/decorators";
+import { declared, property, subclass } from "esri/core/accessorSupport/decorators";
 
 import Accessor = require("esri/core/Accessor");
 import HandleRegistry = require("esri/core/HandleRegistry");
